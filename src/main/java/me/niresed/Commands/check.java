2c04082add8f8d98affb9f2310290f0e6e4dce0f
Niresed <fuadsafarli686@gmail.com>
2023-02-26 11:17:18 +0400

    rtp

--- src/main/java/me/niresed/Commands/check.java
+++ src/main/java/me/niresed/Commands/check.java
@@ -11,24 +11,25 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.object.TownBlock;
 
 import java.util.Objects;
 
 public class check implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        Player player = (Player) sender;
-        Town town = null;
-        try {
-            town = Objects.requireNonNull(TownyAPI.getInstance().getTownBlock(player.getLocation())).getTown();
-        } catch (NotRegisteredException e) {
-            throw new RuntimeException(e);
+        if (sender instanceof Player){
+            Player player = (Player) sender;
+            try {
+                Town town = TownyAPI.getInstance().getTownBlock(player.getLocation()).getTown();
+                if (town != null && town.hasResident(player.getName())) {
+                    //Execute your code here
+                }
+            } catch (NotRegisteredException e) {
+                if (TownyAPI.getInstance().isWilderness(player.getLocation()))
+                    System.out.println("Check Работает");
+            }
         }
-        if (TownyAPI.getInstance().isWilderness(player.getLocation()))
-            System.out.println("Check Работает");
-        if (town != null && town.hasResident(player.getName())) {
-            //Execute your code here
-        }
-        return false;
+        return true;
     }
 }
