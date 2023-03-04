c1e92a0deab05370f2ae7007e177e49e45410d3e
Niresed <fuadsafarli686@gmail.com>
2023-03-04 13:26:50 +0400

    rtp

--- src/main/java/me/niresed/Listeners/RTPEvent.java
+++ src/main/java/me/niresed/Listeners/RTPEvent.java
@@ -4,19 +4,23 @@ import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Town;
 import me.niresed.Commands.RTPUtils;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class RTPEvent implements Listener {
     @EventHandler
-    public void onRTP(PlayerJoinEvent event) {
+    public void onRTP(PlayerRespawnEvent event) {
         Player player = event.getPlayer();
+        System.out.println(player);
         Town town = TownyAPI.getInstance().getTown(player);
+        System.out.println(town);
         if (town == null) {
-            player.teleport(RTPUtils.generateLocation(player));
+            Location s = RTPUtils.generateLocation(player);
+            System.out.println(s);
+            player.teleport(s);
         }
-
     }
 }
