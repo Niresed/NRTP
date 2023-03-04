97c6b79f64ddbda15d6dad8458e68fa5f41828a8
Niresed <fuadsafarli686@gmail.com>
2023-03-04 15:09:13 +0400

    rtp

---
+++ src/main/java/me/niresed/Listeners/RtpJoinEvent.java
@@ -0,0 +1,13 @@
+package me.niresed.Listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class RtpJoinEvent implements Listener {
+    public void onJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+
+    }
+
+}
