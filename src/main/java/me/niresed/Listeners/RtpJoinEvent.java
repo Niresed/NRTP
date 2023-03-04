1d1c7f3e5629ca2a12460901770a458e4a5819f4
Niresed <fuadsafarli686@gmail.com>
2023-03-04 15:27:49 +0400

    rtp

--- src/main/java/me/niresed/Listeners/RtpJoinEvent.java
+++ src/main/java/me/niresed/Listeners/RtpJoinEvent.java
@@ -7,7 +7,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class RtpJoinEvent implements Listener {
     public void onJoin(PlayerJoinEvent event){
         Player player = event.getPlayer();
-
+        System.out.println(player.getPlayerTime());
     }
 
 }
--- src/main/java/me/niresed/Main.java
+++ src/main/java/me/niresed/Main.java
@@ -1,8 +1,8 @@
 package me.niresed;
 
 import me.niresed.Commands.Rtp;
+import me.niresed.Listeners.RtpJoinEvent;
 import me.niresed.Listeners.RtpRespawnEvent;
-import me.niresed.Listeners.Hello;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,8 +13,8 @@ public final class Main extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getServer().getPluginManager().registerEvents(new Hello(), this);
         getServer().getPluginManager().registerEvents(new RtpRespawnEvent(), this);
+        getServer().getPluginManager().registerEvents(new RtpJoinEvent(), this);
         Objects.requireNonNull(getCommand("RTP")).setExecutor(new Rtp());
     }
 }
