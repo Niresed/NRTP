a625e6b2a0a7d509dc6f7de51ee36cc501a304bf
Niresed <fuadsafarli686@gmail.com>
2023-03-25 13:15:41 +0400

    nrtp

--- src/main/java/me/niresed/nrtp/Commands/Rtp.java
+++ src/main/java/me/niresed/nrtp/Commands/Rtp.java
@@ -1,5 +1,6 @@
 package me.niresed.nrtp.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import me.niresed.nrtp.NRTP;
 import org.bukkit.ChatColor;
@@ -14,14 +15,52 @@ public class Rtp implements CommandExecutor {
     Plugin plugin = NRTP.getPlugin(NRTP.class);
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        // ошибки
+        String reasonsError = plugin.getConfig().getString("error reason RTP");
+        String playerError = plugin.getConfig().getString("player error RTP");
+        String commandError = plugin.getConfig().getString("command error RTP");
+        String lackOfUnderCommands = plugin.getConfig().getString("lack of under commands");
         if (sender instanceof Player) {
             Player player = (Player) sender;
-            String errorReasons = plugin.getConfig().getString("error reason RTP");
+
             if(player.hasPermission("nrtp.rtp")) {
-                player.teleport(RtpUtils.generateLocation(player));
-            } else{
-                player.sendMessage(ChatColor.RED + "" + errorReasons);
+                if (args.length == 0){
+                    player.teleport(RtpUtils.generateLocation(player));
+
+                } else if (args.length == 1) {
+                    for(Player onlinePlayers : Bukkit.getOnlinePlayers()){
+                        if (onlinePlayers.getName().equals(args[0])){
+                            onlinePlayers.teleport(RtpUtils.generateLocation(onlinePlayers));
+                            return true;
+                        }
+                    }
+                    player.sendMessage(ChatColor.RED + "" + playerError);
+
+                } else {
+                    player.sendMessage(ChatColor.RED + "" + commandError);
+
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "" + reasonsError);
+
             }
+
+        } else {
+            if (args.length == 0){
+                Bukkit.getLogger().info(ChatColor.RED + "" + lackOfUnderCommands);
+                return true;
+
+            } else if (args.length == 1) {
+                for(Player onlinePlayers : Bukkit.getOnlinePlayers()){
+                    if (onlinePlayers.getName().equals(args[0])){
+                        onlinePlayers.teleport(RtpUtils.generateLocation(onlinePlayers));
+                        return true;
+                    }
+                }
+                Bukkit.getLogger().info(ChatColor.RED + "" + playerError);
+                return true;
+            }
+            Bukkit.getLogger().info(ChatColor.RED + "" + commandError);
 
         }
         return true;
